e16a188292fb81c58ee58596742e6eaff4fb5dfc
haydenagh <131166104+haydenagh@users.noreply.github.com>
2025-01-16 13:17:00 -0500

    Day 2 complete

---
+++ PortfolioWebsite/src/components/Header.vue
@@ -0,0 +1,20 @@
+<script setup>
+    import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+    <div>
+        <div>
+            <RouterLink to="/"><h3>Hayden's Portfolio</h3></RouterLink>
+         </div>
+        <div>
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/about">About</RouterLink>
+            <RouterLink to="/portfolio">Portfolio</RouterLink>
+        </div>
+    </div>
+
+</template>
+
+<style>
+</style>
---
+++ PortfolioWebsite/src/views/About.vue
@@ -0,0 +1,9 @@
+<script setup>
+import topBar from '../components/Header.vue'
+</script>
+
+<template>
+  <main>
+    <topBar />
+  </main>
+</template>
